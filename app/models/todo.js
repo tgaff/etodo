eb9e1da60744340b17d1baef81c9ed71e05295fd
tgaff <tgaff@alumni.nd.edu>
2015-04-29 20:18:58 -0700

    Add fixtures to the todo model

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,6 +1,29 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
+
+// let us hope that this is intended to be a temporary hack
+Todo.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      title: 'Learn Ember.js',
+      isCompleted: true
+    },
+    {
+      id: 2,
+      title: '...',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: 'Profit!',
+      isCompleted: false
+    }
+  ]
+});
+
+export default Todo;
