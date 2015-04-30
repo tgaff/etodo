7119317c172d2697fc8dea08c47fbdba041a6e3e
tgaff <tgaff@alumni.nd.edu>
2015-04-30 14:56:58 -0700

    Add ability to create a new todo and have it saved to the (in memory data store)
    Now following:
    http://thetechcofounder.com/getting-started-with-ember-js-using-ember-cli/

---
+++ app/controllers/todos.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+// from http://thetechcofounder.com/getting-started-with-ember-js-using-ember-cli/
+//
+// note that it may not actually be necessary to use ArrayController
+// the original 'Controller' seemed to work fine
+// TODO: figure out why
+export default Ember.ArrayController.extend({
+  actions: {
+    createTodo: function(newTitle) {
+      //Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: newTitle,
+        isCompleted: false
+      });
+
+      // Clear the "New Todo" text field
+      this.set('newTitle', '');
+
+      // Save the new model
+      todo.save();
+    }
+  }
+});
