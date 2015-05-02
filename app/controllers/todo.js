2a0d68558a4a1d88d69fc0a1fe7351c0db287495
tgaff <tgaff@alumni.nd.edu>
2015-05-01 22:15:02 -0700

    Add the ability to edit and save a title.
    If the user empties the field, we reset it to it's previous value
    instead of saving.
    The original guide, actually does a delete instead but I prefer this.

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -5,6 +5,25 @@ export default Ember.Controller.extend({
     editTodo: function() {
       this.set('isEditing', true);
       console.log("executed editTodo");
+    },
+    acceptChanges: function() {
+      //Remove is editing property
+      this.set('isEditing', false);
+
+      // If the todo is empty, reset it
+      // otherwise save it
+      if(Ember.isEmpty(this.get('model.title'))) {
+        // I tried using this value
+        //origTitle = this.store.getById('todo',thisID).get('title');
+        //this.set('title', origTitle);
+        // but it seems our cache of the model
+        // is instantly updated by ember.  Thanks, ember, I think.
+        // rollback(); works though.
+        this.get('model').rollback();
+      } else {
+        console.log('save[' + this.get('model.title') + ']');
+        this.get('model').save();
+      }
     }
   }
 });
